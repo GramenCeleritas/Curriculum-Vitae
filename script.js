31146922f3d96c5be14bba4f5380ae3cd6271567
admin <vadim771@ukr.net>
2023-10-10 12:57:54 +0300

    js animation added

--- script.js
+++ script.js
@@ -2,7 +2,8 @@
 
 window.onload = function() {
     
-
+        const engLanguage = document.getElementById('Eng')
+        const ukrLanguage = document.getElementById('Ukr')
    
 
         let buttonForChangeLanguage = document.getElementById('buttonLanguageChangerEng')
@@ -10,12 +11,16 @@ window.onload = function() {
             changeLanguageToUkr()
             console.log('you have clicked the mouse on EngButton')
         })
-    
+        
+        
+
         function changeLanguageToUkr() {
-            let engLanguage = document.getElementById('Eng')
-            let ukrLanguage = document.getElementById('Ukr')
             engLanguage.style.display = 'none'
             ukrLanguage.style.display = 'flex'
+            imageOfFaceUkr.classList.remove('scaled');
+            imageOfFaceUkr.classList.remove('unscaled');
+            imageOfFace.classList.remove('unscaled');
+            imageOfFace.classList.remove('scaled');
             
         }
         
@@ -25,19 +30,74 @@ window.onload = function() {
             console.log('you have clicked the mouse on UkrButton')
         })
 
+            
+
         function changeLanguageToEng() {
-            let engLanguage = document.getElementById('Eng')
-            let ukrLanguage = document.getElementById('Ukr')
+            
             engLanguage.style.display = 'flex'
             ukrLanguage.style.display = 'none'
+            console.log((ukrLanguage.style.display == 'none') + ' check is style none')
+            imageOfFaceUkr.classList.remove('scaled');
+            imageOfFaceUkr.classList.remove('unscaled');
+            imageOfFace.classList.remove('unscaled');
+            imageOfFace.classList.remove('scaled');
             
         }
 
-        console.log('page are fully loaded')
+
+
+        const elementsByClass = document.getElementsByClassName('photo')
+
+        console.log(elementsByClass)
     
-        
+        const imageOfFace = document.getElementsByClassName("photo")[0]
+
+        console.log(imageOfFace)
+
+        imageOfFace.addEventListener("click", () => {
+            if (imageOfFace.classList.contains('scaled')) {
+                imageOfFace.classList.remove('scaled');
+                imageOfFace.classList.add('unscaled')
+            } else  {
+                imageOfFace.classList.remove('unscaled');
+                imageOfFace.classList.add('scaled')
+            }
+        })
+
+        imageOfFace.addEventListener("click", () => {
+            console.log('you have clicked on image')
+        } )
+
+        const imageOfFaceUkr = document.getElementsByClassName("photo")[1]
+
+        console.log(imageOfFaceUkr)
+
+        imageOfFaceUkr.addEventListener("click", () => {
+            if (imageOfFaceUkr.classList.contains('scaled')) {
+                imageOfFaceUkr.classList.remove('scaled');
+                imageOfFaceUkr.classList.add('unscaled')
+            } else  {
+                imageOfFaceUkr.classList.remove('unscaled');
+                imageOfFaceUkr.classList.add('scaled')
+            }
+        })
+
+        // reset photo properties 
+
+       
+
+                
+            
         
 
+        
+        console.log(ukrLanguage)
+        imageOfFaceUkr.addEventListener("click", () => {
+            console.log('you have clicked on image')
+        } )
+    
+
+        console.log('page are fully loaded')
     
     
 }
